120fab29480c32762a36ffc92fe41a304b17932f
Zhang Zihan <zhangzihanzhangzihan@live.com>
2024-05-11 18:57:57 +0800

    改进多维 Map 的 get 与 set 方法

---
+++ shared/src/commonMain/kotlin/hamusutax/core/collections/MapSet.kt
@@ -0,0 +1,34 @@
+package hamusutax.core.collections
+
+operator fun <K1, K2, V> MutableMap<K1, MutableMap<K2, V>>.set(key1: K1, key2: K2, value: V) {
+    this[key1]?.let {
+        it[key2] = value
+        return
+    }
+    this[key1] = mutableMapOf(key2 to value)
+}
+
+operator fun <K1, K2, K3, V> MutableMap<K1, MutableMap<K2, MutableMap<K3, V>>>.set(key1: K1, key2: K2, key3: K3, value: V) {
+    this[key1]?.let { value1 ->
+        value1[key2]?.let { value2 ->
+            value2[key3] = value
+            return
+        }
+        value1[key2] = mutableMapOf(key3 to value)
+    }
+    this[key1] = mutableMapOf(key2 to mutableMapOf(key3 to value))
+}
+
+operator fun <K1, K2, K3, K4, V> MutableMap<K1, MutableMap<K2, MutableMap<K3, MutableMap<K4, V>>>>.set(key1: K1, key2: K2, key3: K3, key4: K4, value: V) {
+    this[key1]?.let { value1 ->
+        value1[key2]?.let { value2 ->
+            value2[key3]?.let { value3 ->
+                value3[key4] = value
+                return
+            }
+            value2[key3] = mutableMapOf(key4 to value)
+        }
+        value1[key2] = mutableMapOf(key3 to mutableMapOf(key4 to value))
+    }
+    this[key1] = mutableMapOf(key2 to mutableMapOf(key3 to mutableMapOf(key4 to value)))
+}
